a9b00fd6b54a8705d44616076b536449aaf23977
Romain Caire <romain.caire@doro.com>
2015-03-25 14:34:08 +0100

    Add port

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var pg = require('pg')
 var express = require('express')
-var cool = require('cool-ascii-faces');
+var cool = require('cool-ascii-faces')
 
 var app = express()
+app.set('port', (process.env.PORT || 5000))
 
 app.get('/highscore', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -22,7 +23,7 @@ app.get('/', function (req, res) {
 	res.send(cool())
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get("port"), function () {
 
   var host = server.address().address
   var port = server.address().port
