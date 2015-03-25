9f8339e46b351d04d6098b0281acfd2b39115712
Romain Caire <romain.caire@doro.com>
2015-03-25 14:48:29 +0100

    Add table highscore

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ var cool = require('cool-ascii-faces')
 var app = express()
 app.set('port', (process.env.PORT || 5000))
 
+app.get('/setup_database', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		client.query('CREATE TABLE IF NOT EXISTS highscores (id serial PRIMARY KEY NOT NULL, uuid VARCHAR(255) UNIQUE NOT NULL, name VARCHAR (50) UNIQUE NOT NULL, score integer NOT NULL, timestamp TIMESTAMP)', function(err, result) {
+			done();
+			if (err){ 
+				console.error(err); response.send("Error " + err);
+			} else { 
+				response.send(result);
+			}
+		});
+	});
+})
+
 app.get('/highscore', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
+		client.query('SELECT * FROM highscore ORDER BY score DESC LIMIT 5;', function(err, result) {
 			done();
 			if (err){ 
 				console.error(err); response.send("Error " + err);
@@ -17,7 +30,6 @@ app.get('/highscore', function (request, response) {
 		});
 	});
 })
-
 
 app.get('/', function (req, res) {
 	res.send(cool())
