9f01e59110a91d78451ad7426fd64d1ab02eab03
Romain Caire <romain.caire@doro.com>
2015-03-26 14:52:39 +0100

    sdfsdf

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get('/all_highscore', function (request, response) {
 
 app.get('/uuid_highscore', function (request, response) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT uuid, max(score) AS score FROM highscores GROUP BY uuid;', function(err, result) {
+		client.query('SELECT id FROM highscores WHERE uuid = $1 AND name = $2;', ["uuid", "Romain Caire"], function(err, result) {
 			done();
 			if (err){ 
 				console.error(err); 
